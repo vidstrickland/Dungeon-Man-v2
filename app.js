17ed7c268772e94586c2e5a03a0c1e880bdcdf64
vid Strickland <vidkid7@gmail.com>
2016-07-08 01:19:33 +0000

    initialize dungeon man redevelopment project

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app     = express();
+
+//requiring routes
+var indexRoutes = require("./routes/index");
+
+app.set("view engine", "ejs");
+
+app.use("/", indexRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Dungeon Man server has started");
+});
