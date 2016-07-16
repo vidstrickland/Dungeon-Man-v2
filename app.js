30c6857300e36ead38cc590abb060b6918a500ac
vid Strickland <vidkid7@gmail.com>
2016-07-16 06:45:18 +0000

    add registration functionality (passport) - users can now register and be added to the database

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-var express = require("express"),
-    app     = express();
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user"),
+    methodOverride  = require("method-override");
 
 //requiring routes
 var indexRoutes = require("./routes/index");
 
+//database url
+var url = process.env.DATABASEURL || "mongodb://localhost/dungeon_man"
+mongoose.connect(url);
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"));
+
+//passport configuration
+app.use(require("express-session")({
+    secret: "You are inside of my body. ...Brick Road",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use("/", indexRoutes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,35 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
 
 //Root Route
 router.get("/", function(req, res){
     res.render("landing");
 });
 
+//show login form
 router.get("/login", function (req, res){
     res.render("login");
 });
+
+//show registration form
+router.get("/register", function(req, res){
+   res.render("register");
+});
+
+//registration logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/register");
+        })
+    })
+})
 
 module.exports = router;
